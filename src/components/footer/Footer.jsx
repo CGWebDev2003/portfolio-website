0ccbeaab36ab992346f1dc40288f8d0605e10138
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-07-22 17:23:38 +0200

    Added footer base

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+export default function Footer() {
+    return(
+        <footer className="footer">
+            <div className="footerContent">
+
+            </div>
+            <div className="flowefyBanner">
+                <p className="flowefyBannerText">Designed & Delivered by <a href="https://flowefy.de" target="_blank" className="flowefy">flowefy.</a></p>
+            </div>
+        </footer>
+    );
+}
