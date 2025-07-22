81c196c86527820062fb54f8cc8bf38573166eaa
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-07-22 18:28:26 +0200

    Added Link to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,10 @@ export default function Footer() {
     return(
         <footer className="footer">
             <div className="footerContent">
-
+                <div className="footerCol col1">
+                    <h1 className="footerColTitle">Rechtliches</h1>
+                    <a className="footerLink" href="/impressum" aria-label="Gehe zu Impressum">Impressum</a>
+                </div>
             </div>
             <div className="flowefyBanner">
                 <p className="flowefyBannerText">Designed & Delivered by <a href="https://flowefy.de" target="_blank" className="flowefy">flowefy.</a></p>
