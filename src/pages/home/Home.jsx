039e6d3418e10f574a4c0e722de6d2de23b9ef4b
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-07-22 15:05:09 +0200

    Setup App Router

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import "./home.css";
+
+export default function HomePage() {
+    return(
+        <div className="page homePage">
+            <h1>Home</h1>
+        </div>
+    );
+}
---
+++ src/pages/impressum/Impressum.jsx
@@ -0,0 +1,9 @@
+import "./impressum.css";
+
+export default function ImpressumPage() {
+    return(
+        <div className="page impressumPage">
+            <h1>Impressum</h1>
+        </div>
+    );
+}
