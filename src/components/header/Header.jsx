418bf0008db41d865e5c757bd88b79b53a2a7193
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-07-22 17:04:00 +0200

    Added Poppins font and added Navigation links to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,14 +8,17 @@ export default function Header() {
                 <a href="/" className="logo" aria-label="Gehe zur Homepage">
                     <img src="/cg_signature_black.webp" className="logoImage" alt="Colin Grahm Signatur Logo" />
                 </a>
+                <nav>
+                    <a className="headerNavLink" href="/" aria-label="Gehe zu Home-Seite">Home</a>
+                </nav>
                 <div className="headerButtonBox">
-                    <a className="headerSocialButton" target="_blank" href="https://www.instagram.com/colin_grahm/">
+                    <a className="headerSocialButton" target="_blank" href="https://www.instagram.com/colin_grahm/" aria-label="Gehe zu Colin Grahm's Instagram" title="Instagram">
                         <i className="bi bi-instagram"></i>
                     </a>
-                    <a className="headerSocialButton" target="_blank" href="https://www.linkedin.com/in/colin-grahm-a79422252/">
+                    <a className="headerSocialButton" target="_blank" href="https://www.linkedin.com/in/colin-grahm-a79422252/" aria-label="Gehe zu Colin Grahm's LinkedIn" title="LinkedIn">
                         <i className="bi bi-linkedin"></i>
                     </a>
-                    <a className="headerSocialButton" target="_blank" href="https://github.com/CGWebDev2003">
+                    <a className="headerSocialButton" target="_blank" href="https://github.com/CGWebDev2003" aria-label="Gehe zu Colin Grahm's Git Hub" title="GitHub">
                         <i class="bi bi-github"></i>
                     </a>
                 </div>
