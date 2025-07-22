542f1b2ebc0772c2a1e65f8e32ce938c36660caf
Colin Grahm <122671813+CGWebDev2003@users.noreply.github.com>
2025-07-22 19:08:46 +0200

    Added basic style to global pages and added Impressum data

--- src/pages/impressum/Impressum.jsx
+++ src/pages/impressum/Impressum.jsx
@@ -3,7 +3,22 @@ import "./impressum.css";
 export default function ImpressumPage() {
     return(
         <div className="page impressumPage">
-            <h1>Impressum</h1>
+            <div className="pageContent">
+                <div className="impressumBox">
+                    <h1 className="title">Impressum</h1>
+                    <h2 className="subtitle">Angaben gemäß § 5 TMG:</h2>
+                    <img className="grahmWappen" src="/grahm_wappen.webp" alt="Familieanwappen von Colin Grahm" />
+                    <p className="adress text">
+                        <span className="cgName">Colin Grahm</span><br/>
+                        Schwedenstraße 29A<br/>
+                        04420 Markranstädt<br/>
+                    </p>
+                    <p className="contact text">
+                        Tel: <a href="tel:017624693418">Anrufen</a><br/>
+                        Email: <a href="mailto:colingrahmmedia@gmail.com" aria-label="Email an colingrahmmedia@gmail.com senden" >Email schreiben</a><br/>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
